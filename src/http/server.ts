68bf14178aafc8b474e27eb01ead30e519c00261
Rian Lucas <rianlucas2014@gmail.com>
2024-02-06 09:17:41 -0300

    feat: create polls endpoint

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,27 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+import z from 'zod'
 
 const app = fastify()
 
-app.listen({port: 3333}).then(() => {
+const prisma = new PrismaClient()
+
+app.post('/polls', async (req, res) => {
+  const createPollBody = z.object({
+    title: z.string()
+  })
+
+  const { title } = createPollBody.parse(req.body)
+
+  const poll = await prisma.poll.create({
+    data: {
+      title
+    }
+  })
+
+  return res.status(201).send({ pollId: poll.id })
+})
+
+app.listen({ port: 3333 }).then(() => {
   console.log("Server running!")
 })
